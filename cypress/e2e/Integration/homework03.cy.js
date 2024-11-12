074c06e872daa57ba149d1121c6f22f7357160c4
bsaiakbaeva97 <bsiakabaeva97@gmail.com>
2024-11-12 01:59:10 -0600

    new changes

--- cypress/e2e/Integration/homework03.cy.js
+++ cypress/e2e/Integration/homework03.cy.js
@@ -60,24 +60,6 @@ describe("Book Your Trip From Tests", () => {
 
     })
 
-/*
-Navigate to https://techglobal-training.com/frontend/project-3
-Select the “One way” radio button
-Select “Business” for the “Cabin Class” dropdown
-Select “Illinois” for the “From” dropdown
-Select “Florida” for the “To” dropdown
-Select the next week for the ”Depart”
-Select “1” for the “Number of passengers” dropdown
-Select “Senior (65+)” for the Passenger 1 dropdown
-Click on the “BOOK” button
-Validate the booking information displayed below
-DEPART
-IL to FL
-{dynamic date}
-Number of passengers: 1
-Passenger 1: Senior (65+)
-Cabin Class: Business
-*/
 
     it('Test Case 03 - Validate the booking for 1 passenger and one way', () => {
         bookingPage.getOneWayBtn().check();
@@ -108,32 +90,8 @@ Cabin Class: Business
         
     });
 
-    /*
-        Navigate to https://techglobal-training.com/frontend/project-3
-    Select the “Round trip” radio button
-    Select “First” for the “Cabin Class” dropdown
-    Select “California” for the “From” dropdown
-    Select “Illinois” for the “To” dropdown
-    Select the next week for the ”Depart”
-    Select the next month for the “Return”
-    Select “1” for the “Number of passengers” dropdown
-    Select “Adult (16-64)” for the Passenger 1 dropdown
-    Click on the “BOOK” button
-    Validate the booking information displayed below
-    DEPART
-    CA to IL
-    {dynamic date}
-    Number of passengers: 1
-    Passenger 1: Adult (16-64)
-    Cabin Class: First
 
-
-    RETURN
-    IL to CA
-    {dynamic date}
-    */
-
-    it.only('Test Case 04 - Validate the booking for 1 passenger and round trip', () => {
+    it('Test Case 04 - Validate the booking for 1 passenger and round trip', () => {
 
         bookingPage.getRoundTripBtn().check();
 
@@ -172,26 +130,6 @@ Cabin Class: Business
         });
     });
 
-/*
-Navigate to https://techglobal-training.com/frontend/project-3
-Select the “One way” radio button
-Select “Premium Economy” for the “Cabin Class” dropdown
-Select “New York” for the “From” dropdown
-Select “Texas” for the “To” dropdown
-Select the next day for the ”Depart”
-Select “2” for the “Number of passengers” dropdown
-Select “Adult (16-64)” for the Passenger 1 dropdown
-Select “Child (2-11)” for the Passenger 2 dropdown
-Click on the “BOOK” button
-Validate the booking information displayed below
-DEPART
-NY to TX
-{dynamic date}
-Number of passengers: 2
-Passenger 1: Adult (16-64)
-Passenger 2: Child (2-11)
-Cabin Class: Premium Economy
-*/
 
     it('Test Case 05 - Validate the booking for 2 passengers and one way', () => {
        
